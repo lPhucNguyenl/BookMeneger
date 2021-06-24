202a8878f5a17af32441d5a622a0b56307e997f7
lPhucNguyenl <86363159+lPhucNguyenl@users.noreply.github.com>
2021-06-24 19:54:40 +0700

    Initial commit

---
+++ BookMeneger/Controllers/BooksController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookMeneger.Models;
+
+namespace BookMeneger.Controllers
+{
+    public class BooksController : Controller
+    {
+        
+
+        // GET: Books
+        public ActionResult ListBook()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listbook = context.Books.ToList();
+            return View(listbook);
+        }
+
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if(book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            BookManagerContext context = new BookManagerContext();
+            context.Books.Add(book);
+            context.SaveChanges();
+            return RedirectToAction("ListBook");
+
+        }
+
+
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);    
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book bookUpdate = context.Books.SingleOrDefault(p => p.ID == book.ID);
+            if (bookUpdate != null)
+            {
+                context.Books.Add(bookUpdate);
+                context.SaveChanges();
+            }
+
+            return RedirectToAction("ListBook");
+
+        }
+
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult DeleteBook(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if (book != null)
+            {
+                context.Books.Remove(book);
+                context.SaveChanges();
+            }
+            return RedirectToAction("ListBook");
+        }
+    }
+
+}
+
---
+++ BookMeneger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookMeneger.Startup))]
+namespace BookMeneger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
